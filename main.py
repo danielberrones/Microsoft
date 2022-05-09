18308c857f9b375b4e97324b7e920d56dbbe8f1a
danielberrones <daniel.a.berrones@gmail.com>
2022-05-08 23:36:12 -0500

    refactored

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import yfinance as yf
 
+symbol = 'msft'
+msft = yf.Ticker(symbol)
+
+totalRevenue = msft.get_info()["totalRevenue"]
+
+sep = "{:,}".format(totalRevenue)
+print("totalRevenue\n", sep)
+
+#totalRevenue
+#192,557,006,848
